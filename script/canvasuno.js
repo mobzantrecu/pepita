0d172e9cbf1413082f95d9868efb65c3db2bcea0
andreaibt21 <andreaixabella@gmail.com>
2020-10-25 00:59:05 -0300

    agregué los obstaculos y la colision con los border

--- script/canvasuno.js
+++ script/canvasuno.js
@@ -1,53 +1,84 @@
 
 //MEDIDAS DEL CANVAS
-let canWidthUno =  window.innerWidth * 0.7;
-let canHeightUno = window.innerHeight * 0.8;
+//CAMBIA AL TAMAÑO DEL CANVAS CADA VEZ QUE CAMBIA DE TAMAÑO LA PANTALLA
 
-//ARRANCA EN POSICION
-var xUno= 0;
-var yUno= 0;
-
-//POR DEFECTO TODO EN FALSO
-var leftUno = false;
-var upUno = false;
-var downUno = false;
-
-//LAS POSICIONES DE CADA MOVIMIENTO EN Y
-var trackLeftUno = 2;
-var trackRightUno = 1;
-var trackUpUno = 0;
-var trackDownUno = 3;
-
-var srcXUno;
-var srcYUno;
-
-//TAMAÑO DEL TOTAL DEL LIENZO DE PEPITA
-var sheetWidthUno = 938;
-var sheetHeightUno= 680;
-
-var colsUno = 8;
-var rowsUno = 4;
-
-var widthUno = sheetWidthUno / colsUno; 
-var heightUno = sheetHeightUno / rowsUno; 
-
-var currentFrameUno = 0;
-
-//SE DIBUJA A PEPITA
-var characterUno = new Image()
-characterUno.src = "img/character.png";
-
+function resizeCanvas() {
+    canvasUno.width =  window.innerWidth * 0.7;;
+    canvasUno.height= window.innerHeight * 0.8;
+}
+window.addEventListener("resize", resizeCanvas)
 
 
 //ASIGNO VALORES AL CANVAS
 let canvasUno = document.getElementById("canvasUno");
-canvasUno.width = canWidthUno;
-canvasUno.height= canHeightUno;
+
+canvasUno.width =  window.innerWidth * 0.7;
+canvasUno.height= window.innerHeight * 0.8;
+
 canvasUno.style.background = "blue"
-canvasUno.style.backgroundImage = 'url("img/lab1/fondoAzulito.jpg")'
-canvasUno.style.backgroundSize = 'cover'
+canvasUno.style.backgroundImage = 'url("img/lab1/fondoAzulito.jpg")';
+canvasUno.style.backgroundSize = 'cover';
 let ctxUno= canvasUno.getContext("2d")
 
+//ARRANCA EN POSICION
+let xUno= 0;
+let yUno= 0;
+
+//POR DEFECTO TODO EN FALSO
+let leftUno = false;
+let upUno = false;
+let downUno = false;
+
+//LAS POSICIONES DE CADA MOVIMIENTO EN Y
+let trackLeftUno = 2;
+let trackRightUno = 1;
+let trackUpUno = 0;
+let trackDownUno = 3;
+
+let srcXUno;
+let srcYUno;
+
+//TAMAÑO DEL TOTAL DEL LIENZO DE PEPITA
+let sheetWidthUno = 938;
+let sheetHeightUno= 680;
+
+let colsUno = 8;
+let rowsUno = 4;
+
+let widthUno = sheetWidthUno / colsUno; 
+let heightUno = sheetHeightUno / rowsUno; 
+
+let currentFrameUno = 0;
+
+//SE DIBUJA A PEPITA
+let characterUno = new Image()
+characterUno.src = "img/character.png";
+
+let velocidad = 20;
+
+// las colisiones siempre estan en falso 
+
+let colisionaRight = false;
+let colisionaLeft = false;
+let colisionaUp = false;
+let colisionaDown = false;
+
+// y cuando estan en true es que no se pueden mover
+
+function colisiona() {
+    if (xUno <= 0) {
+        colisionaLeft = true;
+    }
+    if (xUno + widthUno >= canvasUno.width) {
+        colisionaRight = true;
+    } 
+    if (yUno <= 0) {
+        colisionaUp = true;
+    } 
+    if (yUno + heightUno >= canvasUno.height) {
+        colisionaDown = true;
+    }
+}
 
 //LAS FUNCIONES DE MOVIMIENTO, PRIMERO ELIMINA TODO LO QUE ESTE EN EL CANVAS, 
 //LUEGO LE DA VALORES DEPENDIENDO PARA DONDE MOVER, DESPUES SE MUEVE TANTA CANTIDAD DE PIXELES
@@ -56,8 +87,15 @@ function moveRightUno(){
     leftUno = false;
     upUno = false;
     downUno = false;
-    xUno+=20;
-    
+
+    // esto revisa si la colision es falsa y si lo es habilita el movimiento
+
+    if (!colisionaRight) {
+        xUno+=velocidad;
+        colisionaDown = false;
+        colisionaUp = false;
+        colisionaLeft = false;
+    }
 }
 
 function moveLeftUno(){
@@ -65,24 +103,48 @@ function moveLeftUno(){
     leftUno = true;
     upUno = false;
     downUno = false;
-    xUno-=20;            
+    
+    // esto revisa si la colision es falsa y si lo es habilita el movimiento
+
+    if (!colisionaLeft) {
+        xUno-=velocidad;
+        colisionaRight = false;
+        colisionaUp = false;
+        colisionaDown = false;
+    }
+               
 }
 
 function moveDownUno(){
     ctxUno.clearRect(xUno, yUno, widthUno, heightUno);
     upUno = false;
     leftUno= false;
-    downUno = true;
-    yUno+=20;            
+    downUno = true;   
+    
+    // esto revisa si la colision es falsa y si lo es habilita el movimiento
+
+    if (!colisionaDown) {
+        yUno+=velocidad;
+        colisionaRight = false;
+        colisionaUp = false;
+        colisionaLeft = false;
+    }
 }
 function moveUpUno(){
     ctxUno.clearRect(xUno, yUno, widthUno, heightUno);
     upUno= true;
     leftUno = false;
     downUno = false;
-    yUno-=20;            
-}
+    
+    // esto revisa si la colision es falsa y si lo es habilita el movimiento
 
+    if (!colisionaUp) {
+        yUno-=velocidad;
+        colisionaRight = false;
+        colisionaDown = false;
+        colisionaLeft = false;
+    }
+}
 //LA FUNCION QUE ACTUALIZA CONSTANTEMENTE TODO, PRIMERO BORRA TODO EL CANVAS, 
 //DESPUES DIVIDE LOS FRAMES POR LA CANTIDAD DE MOVIMIENTOS EN X, INDICA EN QUE 
 //POSICION DE X ESTA LA IMG, Y DEPENDIENDO DE LOS VALORES DEL MOVIMIENTO INDICA T O F
@@ -97,7 +159,7 @@ function updateFrameUno(){
         }
     if(upUno == true & leftUno == false & downUno == false ){
     
-    srcYUno = trackUpUno * heightUno;
+    srcYUno = trackDownUno * heightUno;
         }
 
     if(upUno == false & leftUno == false & downUno == false){
@@ -105,21 +167,22 @@ function updateFrameUno(){
         }
 
     if(upUno == false & leftUno == false & downUno == true){
-    srcYUno = trackDownUno * heightUno;
+    srcYUno = trackUpUno * heightUno;
         }
-    
-    
+
 }
 
 //LA FUNCION QUE DIBUJA EL CANVAS
 function drawImageUno(){
     updateFrameUno();
-    ctxUno.drawImage(characterUno, srcX, srcY, width, height, x, y, width, height)
+    ctxUno.drawImage(characterUno, srcXUno, srcYUno, widthUno, heightUno, xUno, yUno, widthUno, heightUno)
 }
 
 //DIBUJA EL CANVAS CADA TANTOS INTERVALOS
 setInterval(function(){
     drawImageUno();
+   drawAsteroide()
+   
 }, 100);
 
 
@@ -132,20 +195,21 @@ switch (e.key) {
 case "ArrowUp":
 case "w":
 case "W":
+    colisiona()
     moveUpUno()
-
 break;
 // Abajo
 case "ArrowDown":
 case "s":
 case "S":
+    colisiona()
     moveDownUno()
-
 break;
 // Izquierda
 case "ArrowLeft":
 case "a":
 case "A":
+    colisiona()
     moveLeftUno()
 break;
 
@@ -153,6 +217,7 @@ break;
 case "ArrowRight":
 case "d":
 case "D":
+    colisiona()
     moveRightUno()
 break;
 
@@ -161,6 +226,41 @@ default:
 }
 })
 
+  let obstaculos={
+      x: [/*1*/200, 200, 200, 200, 200, 200, 200, 200,   /*2*/  0,   0,  40, 40,
+       
+        /*3*/ 370, 370, 370,   /*4*/  410, 450, 490, 530, 
+       
+        /*5*/  550, 550, 550, 550, 550, 550, 550,  /*6*/ 710, 710, 710, 710, 710, 710, 710,710,
 
+       ],
+      
+        y: [/*1*/  0,  40,  80, 120, 160, 200, 240, 280, /*2*/  180, 220, 180, 220,  
+       
+        /*3*/  170, 210, 250,  /*4*/  280, 280, 280, 280,  
+       
+        /*5*/  0,  40,  80, 120, 160, 200, 240,   /*6*/ 170, 210, 250, 290, 330, 370, 410, 450,
+    ]
+  };
+
+function drawAsteroide(){
+
+  asteroide = new Image();
+  asteroide.src = 'img/lab1/asteroide.png';
+
+  nave= new Image();
+  nave.src = 'img/lab1/nave.png'
+
+  asteroide.onload = function(){
+      for (let i = 0; i < obstaculos.x.length; i++) {
+        ctxUno.drawImage(asteroide, obstaculos.x[i], obstaculos.y[i], 40, 40);
+          
+      }
+      ctxUno.drawImage(nave, 415, 330, 150, 150);
+   }
+ 
+
+
+}
 
 
